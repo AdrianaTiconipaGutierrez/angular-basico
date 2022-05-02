0e973334cb0ae113b16054db2e41c3923824b68d
Adriana <adrianaticonipa@gmail.com>
2022-05-02 15:55:05 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title:string = 'PRIMERA APP';
+  numero:number = 10;
+
+  sumar(){
+    this.numero = this.numero + 1 ;
+  }
 }
